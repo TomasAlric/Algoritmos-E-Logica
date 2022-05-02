b4b263b99d5245a877452a087543724c741bf226
Tomas Alric <tomas.alricc@gmail.com>
2022-05-02 16:51:54 -0300

    Procedimento - Soma

---
+++ Algoritmos/src/Procedimento/Soma.java
@@ -0,0 +1,14 @@
+package Procedimento;
+
+public class Soma {
+
+    public static void main(String[] args) {
+        System.out.println(soma());
+    }
+
+    public static int soma() {
+        int a;
+        a = 1 + 1;
+        return a;
+    }
+}
